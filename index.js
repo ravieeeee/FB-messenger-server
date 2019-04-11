0e6a1b0bd78d695b5f288b253790ac11880cb726
JuYeong <pyeonjy97@gmail.com>
2019-04-11 21:07:49 +0900

    init

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const exec = require('child_process').exec
+
+app.post('/sendMessage', (req, res) => {
+  const message = req.query.content
+  const recipient = req.query.recipient
+  if (!message || !recipient) return
+
+  const command = 'npx messer --command=\'m "' + recipient + '" ' + message + '\''
+  console.log(command)
+
+  exec(command, (error, stdout, stderr) => {
+    if (!!stdout) { console.log('stdout : %s', stdout) }
+    if (!!stderr) { console.log('stderr : %s', stderr) }
+    if (!!error) { console.log('error : %s', error) }
+    console.log('send!')
+  });
+  res.end()
+})
+
+app.listen(port, () => console.log(`listening on port ${port}!`))
